60c33835e8aec619576087ad61bc42da68b819a3
taylordorin <taylordorin@gmail.com>
2017-06-10 14:29:33 -0400

    began to build splash page top banner and links, added image, logo and nav

--- react/src/containers/MainContainer.js
+++ react/src/containers/MainContainer.js
@@ -9,7 +9,21 @@ class MainContainer extends Component {
   render() {
     return(
       <div>
-      <h1>How about Now?</h1>
+        <div className="background-image">
+          <nav className="clearfix">
+            <div className="right-section">
+              <ul>
+                <li><a href="#" data-text="About Me">About Me</a></li>
+                <li><a href="#" data-text="Experience">Experience</a></li>
+                <li><a href="#" data-text="Portfolio">Portfolio</a></li>
+                <li><a href="#" data-text="Contact Me">Contact Me</a></li>
+              </ul>
+            </div>
+          </nav>
+          <div className="main">
+            <img className="taylor-greeting-img" src={assetHelper["taylor-greeting.png"]}></img>
+          </div>
+        </div>
       </div>
     )
   }
