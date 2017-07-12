807c52db0adb35ed7f054bb231eb73652a5a5c9b
taylordorin <taylordorin@gmail.com>
2017-07-12 17:24:18 -0400

    added the styling for the What I Do Section. Copy is Lorem Ipsum for now

--- react/src/components/WhatIDoTile.js
+++ react/src/components/WhatIDoTile.js
@@ -12,8 +12,33 @@ class WhatIDoTile extends Component {
       <div id="WhatIDo" className="what-i-do-div">
         <div className="main">
           <img className="headlines" src={assetHelper["what-i-do.png"]}></img>
+          <p className="synopsys">Lorem ipsum dolor sit amet, pretium cupidatat neque at id mus sit, in fusce rhoncus. Morbi viverra nonummy orci massa neque, sodales in ipsum consequat, laoreet leo elementum eu suspendisse, senectus velit donec primis, vivamus pellentesque non donec ipsum nulla. </p>
         </div>
-        
+        <div className="row medium-uncollapse large-collapse">
+          <div className="subhead small-4 columns">
+            <div className="main">
+              <img className="what_i_icon" src={assetHelper["webpage.png"]}></img>
+              <h3>web design</h3>
+              <p>Lorem ipsum dolor sit amet, pretium cupidatat neque at id mus sit, in fusce rhoncus. Morbi viverra nonummy orci massa neque, sodales in ipsum.</p>
+            </div>
+          </div>
+
+          <div className="subhead small-4 columns">
+            <div className="main">
+              <img className="what_i_icon" src={assetHelper["design.png"]}></img>
+              <h3>graphic design</h3>
+              <p>Lorem ipsum dolor sit amet, pretium cupidatat neque at id mus sit, in fusce rhoncus. Morbi viverra nonummy orci massa neque, sodales in ipsum.</p>
+            </div>
+          </div>
+
+          <div className="subhead small-4 columns">
+            <div className="main">
+              <img className="what_i_icon" src={assetHelper["thumb_tak.png"]}></img>
+              <h3>project management</h3>
+              <p>Lorem ipsum dolor sit amet, pretium cupidatat neque at id mus sit, in fusce rhoncus. Morbi viverra nonummy orci massa neque, sodales in ipsum.</p>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
