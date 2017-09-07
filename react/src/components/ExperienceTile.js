baf9941d09eb41164b1f42f43cd5d17042f267ff
taylordorin <taylordorin@gmail.com>
2017-09-07 15:48:28 -0400

    begining the even/odd of id for experience class

--- react/src/components/ExperienceTile.js
+++ react/src/components/ExperienceTile.js
@@ -8,8 +8,14 @@ class ExperienceTile extends Component {
   }
 
 
-
   render() {
+    let redBlue;
+    if (this.props.id % 2 === 0) {
+      redBlue = "red-title";
+    } else {
+      redBlue = "brown-title";
+    }
+
     let bullet_points = this.props.description.map(
       (bullet_point, index) => {
       return(
@@ -23,7 +29,7 @@ class ExperienceTile extends Component {
       <div className="timeline">
         <div className="entry">
           <div className="title">
-            <h3>{this.props.company}</h3>
+            <h3 className={redBlue}>{this.props.company}</h3>
             <p>{this.props.start_date} - {this.props.end_date}</p>
           </div>
           <div className="body">
@@ -40,3 +46,10 @@ class ExperienceTile extends Component {
 export default ExperienceTile;
 
 // <p>{this.props.description}</p>
+
+// let hidden;
+// if (this.props.id % 1 === 0) {
+//   hidden = 'r-title';
+// } else {
+//   hidden = 'title';
+// }
