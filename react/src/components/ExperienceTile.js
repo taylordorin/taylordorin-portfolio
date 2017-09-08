2eb015bea9ec26278876d3deeb2a3daeed4f9176
taylordorin <taylordorin@gmail.com>
2017-09-08 12:58:40 -0400

    changing the style of the experience

--- react/src/components/ExperienceTile.js
+++ react/src/components/ExperienceTile.js
@@ -27,7 +27,7 @@ class ExperienceTile extends Component {
     })
     return(
       <div className="timeline">
-        <div className="entry">
+        <div className="entry ">
           <div className="title">
             <h3 className={redBlue}>{this.props.company}</h3>
             <p>{this.props.start_date} - {this.props.end_date}</p>
@@ -52,4 +52,49 @@ export default ExperienceTile;
 //   hidden = 'r-title';
 // } else {
 //   hidden = 'title';
+// }
+<section id="" class="">
+  <div class="">
+
+    <div class="container">
+      <div class="expirience">
+        <div class="expirience-item">
+          <div class="icon-b"></div>
+          <div class="expirience-item-inner">
+            <div class="date">March 2017 - May 2017</div>
+            <h3>Apprentice React.js Developer</h3>
+            <p>Lorem ipsum Cillum irure in dolor laboris culpa incididunt sint adipisicing nisi ullamco dolor enim aute do officia adipisicing enim laborum nostrud minim ut fugiat eiusmod commodo non culpa nisi incididunt minim esse aliqua laboris Excepteur est eu quis nisi deserunt id.</p>
+          </div>
+        </div>
+        <div class="expirience-item">
+          <div class="icon-b"></div>
+          <div class="expirience-item-inner">
+            <div class="date">February 2013 - January 2017</div>
+            <h3>Project Manager</h3>
+            <p>Lorem ipsum Anim veniam irure dolor irure dolor incididunt aute ut ea anim dolor cupidatat amet eu consequat velit ea occaecat sed mollit in do labore incididunt Duis nostrud est id labore veniam veniam non cupidatat ullamco et culpa voluptate consequat nulla.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</section>
+
+// return(
+  // <div className="timeline">
+  //   <div className="entry">
+  //     <div className="title">
+  //       <h3 className={redBlue}>{this.props.company}</h3>
+  //       <p>{this.props.start_date} - {this.props.end_date}</p>
+  //     </div>
+  //     <div className="body">
+  //       <h3>{this.props.position}</h3>
+  //       <ul>
+  //         {bullet_points}
+  //       </ul>
+  //     </div>
+  //   </div>
+  // </div>
+// )
+// }
 // }
