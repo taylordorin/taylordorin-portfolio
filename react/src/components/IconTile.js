780977009a00debd0f59ae04114be457a271cbc4
taylordorin <taylordorin@gmail.com>
2017-09-13 16:08:26 -0400

    changed out the coroflot logo, created a link to open an email window for the let's talk button

--- react/src/components/IconTile.js
+++ react/src/components/IconTile.js
@@ -11,7 +11,7 @@ class IconTile extends Component {
       <div>
         <a href="https://github.com/taylordorin"><img className="icons" src={assetHelper["github-logo.png"]}></img></a>
         <a href="https://www.linkedin.com/in/taylor-ross-dorin/"><img className="icons" src={assetHelper["linked-in-logo.png"]}></img></a>
-        <a href="http://www.coroflot.com/taylordorin"><img className="icons" src={assetHelper["coroflot-logo.png"]}></img></a>
+        <a href="http://www.coroflot.com/taylordorin"><img className="icons" src={assetHelper["coroflot-logo-2.png"]}></img></a>
       </div>
     )
   }
